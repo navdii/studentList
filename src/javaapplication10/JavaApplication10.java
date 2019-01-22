3fd2ec44dc55056f9747b18d98ec68cae58861ac
Deep-Navadiya-Sheridan <navadiyd@ssheridancollege.ca>
2019-01-22 17:06:01 -0500

    Added the date of birth and its getter and setter to the student class

--- src/javaapplication10/JavaApplication10.java
+++ src/javaapplication10/JavaApplication10.java
@@ -12,17 +12,6 @@ public class JavaApplication10 {
     public static void main(String[] args) {
         
     }
-
-    /**
-     * @return the dob
-     */
-    public int getDob() {
-        return dob;
-    }
-
-    /**
-     * @param dob the dob to set
-     */
     public void setDob(int dob) {
         this.dob = dob;
     }
